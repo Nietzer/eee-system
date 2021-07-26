a484d7ff41709fe1b27427ae5a6e1f10ddd90215
Nietzer <nei.bier@mail.ru>
2021-07-26 12:53:43 +0300

    Подправка vedor, .htaccess

--- models/PlanForm.php
+++ models/PlanForm.php
@@ -17,6 +17,14 @@ use Yii;
  * @property int $type_id
  * @property string $file
  * @property int $status_id
+ *
+ * @property Speciality $speciality
+ * @property Subject $subject
+ * @property Type $type
+ * @property Cathedra $cathedra
+ * @property Status $status
+ * @property Author $id0
+ * @property Speciality $speciality0
  */
 class PlanForm extends \yii\db\ActiveRecord
 {
@@ -34,10 +42,16 @@ class PlanForm extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['name', 'date_create', 'date_term', 'cathedra_id', 'speciality_id', 'subject_id', 'type_id', 'file', 'status_id'], 'required'],
+            [['name', 'date_term', 'cathedra_id', 'speciality_id', 'subject_id', 'type_id', 'file', 'status_id'], 'required'],
             [['date_create', 'date_term'], 'safe'],
             [['cathedra_id', 'speciality_id', 'subject_id', 'type_id', 'status_id'], 'integer'],
             [['name', 'file'], 'string', 'max' => 255],
+            [['speciality_id'], 'exist', 'skipOnError' => true, 'targetClass' => Speciality::className(), 'targetAttribute' => ['speciality_id' => 'id']],
+            [['subject_id'], 'exist', 'skipOnError' => true, 'targetClass' => Subject::className(), 'targetAttribute' => ['subject_id' => 'id']],
+            [['type_id'], 'exist', 'skipOnError' => true, 'targetClass' => Type::className(), 'targetAttribute' => ['type_id' => 'id']],
+            [['cathedra_id'], 'exist', 'skipOnError' => true, 'targetClass' => Cathedra::className(), 'targetAttribute' => ['cathedra_id' => 'id']],
+            [['status_id'], 'exist', 'skipOnError' => true, 'targetClass' => Status::className(), 'targetAttribute' => ['status_id' => 'id']],
+            [['id'], 'exist', 'skipOnError' => true, 'targetClass' => Author::className(), 'targetAttribute' => ['id' => 'plan_id']],
         ];
     }
 
@@ -58,5 +72,75 @@ class PlanForm extends \yii\db\ActiveRecord
             'file' => 'File',
             'status_id' => 'Status ID',
         ];
+    }
+
+    /**
+     * Gets query for [[Speciality]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSpeciality()
+    {
+        return $this->hasOne(Speciality::className(), ['id' => 'speciality_id']);
+    }
+
+    /**
+     * Gets query for [[Subject]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSubject()
+    {
+        return $this->hasOne(Subject::className(), ['id' => 'subject_id']);
+    }
+
+    /**
+     * Gets query for [[Type]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getType()
+    {
+        return $this->hasOne(Type::className(), ['id' => 'type_id']);
+    }
+
+    /**
+     * Gets query for [[Cathedra]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCathedra()
+    {
+        return $this->hasOne(Cathedra::className(), ['id' => 'cathedra_id']);
+    }
+
+    /**
+     * Gets query for [[Status]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStatus()
+    {
+        return $this->hasOne(Status::className(), ['id' => 'status_id']);
+    }
+
+    /**
+     * Gets query for [[Id0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getId0()
+    {
+        return $this->hasOne(Author::className(), ['plan_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Speciality0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSpeciality0()
+    {
+        return $this->hasOne(Speciality::className(), ['id' => 'speciality_id']);
     }
 }
